c7eaaefce895378395723d18cfab3855c305daa8
matthew jording <mjording@opengotham.com>
2011-01-06 19:55:02 -0500

    key_code spec

---
+++ 001/meat-pizza/key_code_generator_spec.rb
@@ -0,0 +1,32 @@
+# Pull in some spec settings.
+require File.join( File.dirname(__FILE__), '..', '..', 'spec_helper' )
+
+# 
+# 
+
+
+# Pull in all the Ruby files in this directory.
+Dir.glob( File.join( File.dirname(__FILE__), '*.rb' ) ).each do |file|
+  require file unless file.match /_spec.rb$/
+end
+
+# Here is the problem statement:
+# generate 5000 random alpha numeric key codes
+# do not use these characters
+# 0, 1, 2, o, l, z
+describe KeyCodeGenerator do
+
+  @excludes = ['o','z','l',0,2,1]
+  
+  it "should not have any excludes" do
+
+    @reports << Benchmark.measure do
+      @keycodes = KeyCodeGenerator.generate
+      (@all & @excludes).length.should == 5000
+    end
+  end
+
+end 
+
+
+
